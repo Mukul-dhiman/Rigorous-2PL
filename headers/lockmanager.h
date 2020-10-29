d8a06f2807bdd5c4aa1d6cd19675c6b7e7cae6aa
Mukul-dhiman <orton1615@gmail.com>
2020-10-30 01:48:06 +0530

    adding methods for read/write lock

--- headers/lockmanager.h
+++ headers/lockmanager.h
@@ -13,28 +13,51 @@ class lockmanager
         // sequence of operations in threads
         vector<string> Sequiential_output;
 
-        lockmanager(map<string, int> state_variables){
-            read_count.clear();
+        // queue of transaction to acquire locks for every state variable 
+        // queue of pairs -> ( state variable )
+        unordered_map<string,queue<int>> state_queue_lock;
 
+        lockmanager(map<string, int> state_variables){
+
+            read_count.clear();
             lock_state.clear();
             for(auto state:state_variables){
                 lock_state[state]=0;
                 read_count[state]=0;
             }
-
+            state_queue_lock.clear();
             Sequiential_output.clear();
         }
-        void Read_lock(int transaction_id,string state_variable){
+        bool Read_lock(int transaction_id,string state_variable){
             if(lock_state[state_variable]==0){
                 lock_state[state_variable]=1;
                 read_count[state]++;
+                return true;
             }
             else if(lock_state[state_variable]==1){
-                continue;
+                return true;
             }
             else{
-
+                state_queue_lock[state_variable].push(transaction_id);
+                return false;
             }
         }
 
+        bool Write_lock(int transaction_id,string state_variable){
+            if(read_count[state_variable]!=0){
+                state_queue_lock[state_variable].push(transaction_id);
+                return false;
+            }
+            else{
+                if(state_queue_lock[state_variable].size()!=0){
+                    state_queue_lock[state_variable].push(transaction_id);
+                    return false;
+                }
+                else{
+                    lock_state[state_variable]=2;
+                    return true;
+                }
+            }
+        }
+        
 };
