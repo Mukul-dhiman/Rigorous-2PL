af8aa59d34f5d9a019c2016fc3b00de363025409
Mukul-dhiman <orton1615@gmail.com>
2020-10-30 01:11:21 +0530

    converting read count form int to vector od state read count

--- headers/lockmanager.h
+++ headers/lockmanager.h
@@ -1,8 +1,8 @@
 class lockmanager
 {
     public:
-        // current number of reading threads
-        int read_count;
+        // current number of reading threads on a state
+        unordered_map<string,int> read_count;
 
         // read_lock, write_lock and none status for every state
         // none -> "0"
@@ -14,15 +14,27 @@ class lockmanager
         vector<string> Sequiential_output;
 
         lockmanager(map<string, int> state_variables){
-            read_count=0;
+            read_count.clear();
 
             lock_state.clear();
             for(auto state:state_variables){
                 lock_state[state]=0;
+                read_count[state]=0;
             }
+
+            Sequiential_output.clear();
         }
         void Read_lock(int transaction_id,string state_variable){
-            // if(lock_state.find(state_variable)==lock)
+            if(lock_state[state_variable]==0){
+                lock_state[state_variable]=1;
+                read_count[state]++;
+            }
+            else if(lock_state[state_variable]==1){
+                continue;
+            }
+            else{
+
+            }
         }
 
 };
