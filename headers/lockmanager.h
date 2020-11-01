44c150fa77656bfb055ac7d131c109383857e21b
PrakharUniyal <b18128@students.iitmandi.ac.in>
2020-11-01 19:40:32 +0530

    Change the locks with CVs

--- headers/lockmanager.h
+++ headers/lockmanager.h
@@ -1,11 +1,11 @@
-#include <semaphore.h>
+#include <pthread.h>
 #include <bits/stdc++.h>
 
 class lockmanager
 {
-    sem_t lock_manager_lock;
+    pthread_mutex_t lock_manager_lock;
     std::map<std::string, int> var_map;
-    std::vector<sem_t> variable_write_locks;
+    std::vector<pthread_cond_t> variable_write_locks;
     std::vector<bool> variable_is_write_locked;
     std::vector<std::multiset<int>> transaction_set;
 
@@ -17,15 +17,14 @@ public:
     
     lockmanager(std::vector<std::string> state_variables)
     {
-        sem_init(&lock_manager_lock, 0, 1);
-
-        variable_write_locks = std::vector<sem_t>(state_variables.size());
+        lock_manager_lock = PTHREAD_MUTEX_INITIALIZER;
+        variable_write_locks = std::vector<pthread_cond_t>(state_variables.size());
         variable_is_write_locked = std::vector<bool>(state_variables.size(),false);
 
         for (int i = 0; i < state_variables.size(); i++)
         {
             var_map[state_variables[i]] = i;
-            sem_init(&variable_write_locks[i], 0, 1);
+            variable_write_locks[i] = PTHREAD_COND_INITIALIZER;
         }
 
         transaction_set = std::vector<std::multiset<int>>(state_variables.size());
@@ -33,87 +32,74 @@ public:
 
     void read_lock_acquire(int t_id, std::string var_name)
     {
-        sem_wait(&lock_manager_lock);
-        transaction_set[var_map[var_name]].insert(t_id);
-        if (transaction_set[var_map[var_name]].size() == 1)
+        pthread_mutex_lock(&lock_manager_lock);
+        while(variable_is_write_locked[var_map[var_name]])
         {
-            if(variable_is_write_locked[var_map[var_name]])
-                std::cout << "wait_R-lock" << t_id << "," << var_name << "]" << std::endl;
-            sem_wait(&variable_write_locks[var_map[var_name]]);
-            variable_is_write_locked[var_map[var_name]] = true;
+            std::cout << "wait_R-lock" << t_id << "," << var_name << "]" << std::endl;
+            pthread_cond_wait(&variable_write_locks[var_map[var_name]], &lock_manager_lock);
         }
+        transaction_set[var_map[var_name]].insert(t_id);
         std::cout << "R-lock" << "[" << t_id << "," << var_name << "]" << std::endl;
-        sem_post(&lock_manager_lock);
+        pthread_mutex_unlock(&lock_manager_lock);
     }
 
     void read_lock_release(int t_id, std::string var_name)
     {
-        sem_wait(&lock_manager_lock);
+        pthread_mutex_lock(&lock_manager_lock);
         transaction_set[var_map[var_name]].erase(t_id);
-        if (transaction_set[var_map[var_name]].size() == 0)
-        {
-            sem_post(&variable_write_locks[var_map[var_name]]);
-            variable_is_write_locked[var_map[var_name]] = false;
-        }
-        std::cout << "unlock" << "[" << t_id << "," << var_name << "]" << std::endl;
-        sem_post(&lock_manager_lock);
+        std::cout << "R-unlock" << "[" << t_id << "," << var_name << "]" << std::endl;
+        pthread_cond_signal(&variable_write_locks[var_map[var_name]]);
+        pthread_mutex_unlock(&lock_manager_lock);
     }
 
     void write_lock_acquire(int t_id, std::string var_name)
     {
-        sem_wait(&lock_manager_lock);
-        if(variable_is_write_locked[var_map[var_name]])
+        pthread_mutex_lock(&lock_manager_lock);
+        while(variable_is_write_locked[var_map[var_name]] || transaction_set[var_map[var_name]].size()!=0)
+        {
             std::cout << "wait_W-lock" << "[" << t_id << "," << var_name << "]" << std::endl;
-        sem_post(&lock_manager_lock);
-        sem_wait(&variable_write_locks[var_map[var_name]]);
-        sem_wait(&lock_manager_lock);
+            pthread_cond_wait(&variable_write_locks[var_map[var_name]],&lock_manager_lock);
+        }
         variable_is_write_locked[var_map[var_name]] = true;
         std::cout << "W-lock" << "[" << t_id << "," << var_name << "]" << std::endl;
-        sem_post(&lock_manager_lock);
+        pthread_mutex_unlock(&lock_manager_lock);
     }
 
     void write_lock_release(int t_id, std::string var_name)
     {
-        sem_post(&variable_write_locks[var_map[var_name]]);
-        sem_wait(&lock_manager_lock);
+        pthread_mutex_lock(&lock_manager_lock);
+        std::cout << "W-unlock" << "[" << t_id << "," << var_name << "]" << std::endl;
         variable_is_write_locked[var_map[var_name]] = false;
-        std::cout << "unlock" << "[" << t_id << "," << var_name << "]" << std::endl;
-        sem_post(&lock_manager_lock);
+        pthread_cond_signal(&variable_write_locks[var_map[var_name]]);
+        pthread_mutex_unlock(&lock_manager_lock);
     }
 
     void upgrade_lock(int t_id, std::string var_name)
     {
-        sem_wait(&lock_manager_lock);
+        pthread_mutex_lock(&lock_manager_lock);
         transaction_set[var_map[var_name]].erase(t_id);
-        if (transaction_set[var_map[var_name]].size() == 0)
+        while (variable_is_write_locked[var_map[var_name]] || transaction_set[var_map[var_name]].size() != 0)
         {
-            sem_post(&variable_write_locks[var_map[var_name]]);
-            variable_is_write_locked[var_map[var_name]] = false;
-        }
-        if(variable_is_write_locked[var_map[var_name]])
             std::cout << "wait_upgrade" << "[" << t_id << "," << var_name << "]" << std::endl;
-        sem_post(&lock_manager_lock);
-        sem_wait(&variable_write_locks[var_map[var_name]]);
-        sem_wait(&lock_manager_lock);
+            pthread_cond_wait(&variable_write_locks[var_map[var_name]], &lock_manager_lock);
+        }
         variable_is_write_locked[var_map[var_name]] = true;
         std::cout << "upgrade" << "[" << t_id << "," << var_name << "]" << std::endl;
-        sem_post(&lock_manager_lock);
+        pthread_mutex_unlock(&lock_manager_lock);
     }
 
     void downgrade_lock(int t_id, std::string var_name)
     {
-        sem_post(&variable_write_locks[var_map[var_name]]);
-        sem_wait(&lock_manager_lock);
+        pthread_mutex_lock(&lock_manager_lock);
         variable_is_write_locked[var_map[var_name]] = false;
-        transaction_set[var_map[var_name]].insert(t_id);
-        if (transaction_set[var_map[var_name]].size() == 1)
+        pthread_cond_signal(&variable_write_locks[var_map[var_name]]);
+        while (variable_is_write_locked[var_map[var_name]])
         {
-            if(variable_is_write_locked[var_map[var_name]])
-                std::cout << "wait_downgrade" << "[" << t_id << "," << var_name << "]" << std::endl;
-            sem_wait(&variable_write_locks[var_map[var_name]]);
-            variable_is_write_locked[var_map[var_name]] = true;
+            std::cout << "wait_downgrade" << t_id << "," << var_name << "]" << std::endl;
+            pthread_cond_wait(&variable_write_locks[var_map[var_name]], &lock_manager_lock);
         }
+        transaction_set[var_map[var_name]].insert(t_id);
         std::cout << "downgrade" << "[" << t_id << "," << var_name << "]" << std::endl;
-        sem_post(&lock_manager_lock);
+        pthread_mutex_unlock(&lock_manager_lock);
     }
 };
