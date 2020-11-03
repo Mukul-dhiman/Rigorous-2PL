63a241df8920ec0e8f6751bae673314c2cd7c0b2
Mukul-dhiman <orton1615@gmail.com>
2020-11-03 10:23:29 +0530

    adding text files

--- headers/lockmanager.h
+++ headers/lockmanager.h
@@ -1,4 +1,110 @@
+#include <pthread.h>
+#include <bits/stdc++.h>
+
 class lockmanager
 {
+    pthread_mutex_t lock_manager_lock;
+    std::map<std::string, int> var_map;
+    std::vector<pthread_cond_t> variable_write_locks;
+    std::vector<bool> variable_is_write_locked;
+    std::vector<std::multiset<int>> transaction_set;
+    std::vector<std::string> logs;
 
+public:
+    lockmanager()
+    {
+    }
+
+    lockmanager(std::vector<std::string> state_variables)
+    {
+        lock_manager_lock = PTHREAD_MUTEX_INITIALIZER;
+        variable_write_locks = std::vector<pthread_cond_t>(state_variables.size());
+        variable_is_write_locked = std::vector<bool>(state_variables.size(), false);
+
+        for (int i = 0; i < state_variables.size(); i++)
+        {
+            var_map[state_variables[i]] = i;
+            variable_write_locks[i] = PTHREAD_COND_INITIALIZER;
+        }
+
+        transaction_set = std::vector<std::multiset<int>>(state_variables.size());
+    }
+
+    void read_lock_acquire(int t_id, std::string var_name)
+    {
+        pthread_mutex_lock(&lock_manager_lock);
+        while (variable_is_write_locked[var_map[var_name]])
+        {
+            logs.push_back("wait_R-lock[" + std::to_string(t_id) + "," + var_name + "]");
+            pthread_cond_wait(&variable_write_locks[var_map[var_name]], &lock_manager_lock);
+        }
+        transaction_set[var_map[var_name]].insert(t_id);
+        logs.push_back("R-lock[" + std::to_string(t_id) + "," + var_name + "]");
+        pthread_mutex_unlock(&lock_manager_lock);
+    }
+
+    void read_lock_release(int t_id, std::string var_name)
+    {
+        pthread_mutex_lock(&lock_manager_lock);
+        transaction_set[var_map[var_name]].erase(t_id);
+        logs.push_back("R-unlock[" + std::to_string(t_id) + "," + var_name + "]");
+        pthread_cond_signal(&variable_write_locks[var_map[var_name]]);
+        pthread_mutex_unlock(&lock_manager_lock);
+    }
+
+    void write_lock_acquire(int t_id, std::string var_name)
+    {
+        pthread_mutex_lock(&lock_manager_lock);
+        while (variable_is_write_locked[var_map[var_name]] || transaction_set[var_map[var_name]].size() != 0)
+        {
+            logs.push_back("wait_W-lock[" + std::to_string(t_id) + "," + var_name + "]");
+            pthread_cond_wait(&variable_write_locks[var_map[var_name]], &lock_manager_lock);
+        }
+        variable_is_write_locked[var_map[var_name]] = true;
+        logs.push_back("W-lock[" + std::to_string(t_id) + "," + var_name + "]");
+        pthread_mutex_unlock(&lock_manager_lock);
+    }
+
+    void write_lock_release(int t_id, std::string var_name)
+    {
+        pthread_mutex_lock(&lock_manager_lock);
+        logs.push_back("W-unlock[" + std::to_string(t_id) + "," + var_name + "]");
+        variable_is_write_locked[var_map[var_name]] = false;
+        pthread_cond_signal(&variable_write_locks[var_map[var_name]]);
+        pthread_mutex_unlock(&lock_manager_lock);
+    }
+
+    void upgrade_lock(int t_id, std::string var_name)
+    {
+        pthread_mutex_lock(&lock_manager_lock);
+        transaction_set[var_map[var_name]].erase(t_id);
+        while (variable_is_write_locked[var_map[var_name]] || transaction_set[var_map[var_name]].size() != 0)
+        {
+            logs.push_back("wait_upgrade[" + std::to_string(t_id) + "," + var_name + "]");
+            pthread_cond_wait(&variable_write_locks[var_map[var_name]], &lock_manager_lock);
+        }
+        variable_is_write_locked[var_map[var_name]] = true;
+        logs.push_back("upgrade[" + std::to_string(t_id) + "," + var_name + "]");
+        pthread_mutex_unlock(&lock_manager_lock);
+    }
+
+    void downgrade_lock(int t_id, std::string var_name)
+    {
+        pthread_mutex_lock(&lock_manager_lock);
+        variable_is_write_locked[var_map[var_name]] = false;
+        pthread_cond_signal(&variable_write_locks[var_map[var_name]]);
+        while (variable_is_write_locked[var_map[var_name]])
+        {
+            logs.push_back("wait_downgrade[" + std::to_string(t_id) + "," + var_name + "]");
+            pthread_cond_wait(&variable_write_locks[var_map[var_name]], &lock_manager_lock);
+        }
+        transaction_set[var_map[var_name]].insert(t_id);
+        logs.push_back("downgrade[" + std::to_string(t_id) + "," + var_name + "]");
+        pthread_mutex_unlock(&lock_manager_lock);
+    }
+
+    std::vector<std::string> get_logs()
+    {
+        return logs;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@
 #include "headers/lockmanager.h"
 using namespace std;
 
+string inputfile, outputfile;
+map<string, int> state_variables_list_Initial;
 map<string, int> state_variables;
+vector<string> state_variable_list;
+lockmanager L;
+vector<transaction> transaction_array;
 
 // R(X)
 int get_value(string variable_name)
@@ -37,11 +42,129 @@ vector<string> split_string(string s)
     return v;
 }
 
+void operation_eval(map<string, int> &variables, vector<string> op_tokens)
+{
+    // Structure of statement: lval = rval1 op rval2
+    // lval: a var_name which may or may not exist in local_copy
+    // rvali: var_name which exists in local_copy or constant
+    // op: + or -
+    string lval = op_tokens[0];
+    string rval1 = op_tokens[2];
+    string rval2 = op_tokens[4];
+    string op = op_tokens[3];
+
+    int rval = 0;
+
+    if (variables.find(rval1) == variables.end())
+        rval += stoi(rval1);
+    else
+        rval += variables[rval1];
+
+    int op_sign = 2 * (op[0] == '+') - 1;
+
+    if (variables.find(rval2) == variables.end())
+        rval += (op_sign * stoi(rval2));
+    else
+        rval += (op_sign * variables[rval2]);
+
+    variables[lval] = rval;
+}
+
 // TODO
 void *execute_transaction(void *arg)
 {
-    // int i = (long long int)arg;
-    // cout << i << endl;
+    int id = (long long int)arg+1;
+    transaction t = transaction_array[id-1];
+    // t.debug();
+    vector<string> ops = t.get_list_of_operations();
+    map<string, string> locks = t.get_locks_acquired();
+    map<string, int> local_var_copies;
+
+    if (t.get_outcome() == t_abort)
+        return NULL;
+
+    vector<string> tokens;
+    for (int i = 0; i < ops.size(); i++)
+    {
+        tokens = split_string(ops[i]);
+        
+        // If read operation
+        if (tokens[0] == "R")
+        {
+            string lock_var = tokens[1];
+
+            // If no lock acquired.
+            if (locks.find(lock_var) == locks.end())
+            {
+                L.read_lock_acquire(id, lock_var);
+            }
+            else
+            {
+                // If write lock acquired.
+                if (locks[lock_var] == "W")
+                {
+                    L.downgrade_lock(id, lock_var);
+                }
+                // If read lock acquired.
+                else
+                {
+                    // Do nothing.
+                }
+            }
+
+            locks[lock_var] = "R";
+
+            // Read.
+            local_var_copies[lock_var] = get_value(lock_var);
+        }
+        // If write operation
+        else if (tokens[0] == "W")
+        {
+            string lock_var = tokens[1];
+
+            // If no lock acquired.
+            if (locks.find(lock_var) == locks.end())
+            {
+                L.write_lock_acquire(id, lock_var);
+            }
+            else
+            {
+                // If read lock acquired.
+                if (locks[lock_var] == "R")
+                {
+                    L.upgrade_lock(id, lock_var);
+                }
+                // If write lock acquired.
+                else
+                {
+                    // Do nothing.
+                }
+            }
+
+            locks[lock_var] = "W";
+
+            // Write
+            set_value(lock_var, local_var_copies[lock_var]);
+        }
+        // Operation.
+        else
+        {
+            operation_eval(local_var_copies, tokens);
+        }
+    }
+
+    for (auto lock : locks)
+    {
+        if (lock.second == "R")
+        {
+            L.read_lock_release(id, lock.first);
+        }
+        else
+        {
+            L.write_lock_release(id, lock.first);
+        }
+    }
+
     return NULL;
 }
 
@@ -55,36 +178,38 @@ int main(int argc, char *argv[])
     }
 
     // Redirect I/O to files provided as argument
-    ifstream cin(argv[1]);
-    ofstream cout(argv[2]);
+    inputfile = argv[1];
+    ifstream cin(inputfile);
+    outputfile = argv[2];
+    ofstream cout(outputfile);
 
     // Number of transactions
     string read_line;
     getline(cin, read_line);
     int N = stoi(read_line);
 
-    cout << N << endl;
-
     // State variables
     getline(cin, read_line);
-    vector<string> state_variable_list = split_string(read_line);
+    vector<string> state_variable_line = split_string(read_line);
 
-    for (int i = 0; i < state_variable_list.size(); i += 2)
+    for (int i = 0; i < state_variable_line.size(); i += 2)
     {
-        state_variables[state_variable_list[i]] = stoi(state_variable_list[i + 1]);
+        state_variable_list.push_back(state_variable_line[i]);
+        state_variables[state_variable_line[i]] = stoi(state_variable_line[i + 1]);
     }
 
-    for (auto it : state_variables)
-    {
-        cout << it.first << ": " << it.second << endl;
-    }
+    // for(auto var: state_variables)
+    // {
+    //     cout<<var.first<<": "<<var.second<<endl;
+    // }
+    
+    state_variables_list_Initial = state_variables;
+
+    L = lockmanager(state_variable_list);
 
     // Transactions
-    vector<transaction> transaction_array;
     for (int i = 0; i < N; i++)
     {
-        cout << endl;
-
         int transaction_id;
         vector<string> operation_list;
         outcome oc;
@@ -95,7 +220,6 @@ int main(int argc, char *argv[])
         getline(cin, read_line);
         while (read_line != "C" && read_line != "A")
         {
-            cout << read_line << endl;
             operation_list.push_back(read_line);
             getline(cin, read_line);
         }
@@ -120,5 +244,19 @@ int main(int argc, char *argv[])
         pthread_join(tid[i], NULL);
     }
 
+    for(auto var: state_variables)
+    {
+        cout<<var.first<<": "<<var.second<<" ";
+    }
+    cout<<endl;
+
+    vector<string> logs = L.get_logs();
+    for(auto log: logs)
+    {
+        cout<<log<<endl;
+    }
+
+    cout<<"Successfully executed all the transactions."<<endl;
+    
     return 0;
 }
